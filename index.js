4c00c1478fb50ea0e96fdcd808894572cb7a8d2f
ZionMGitHub <zionman33@gmail.com>
2022-06-21 16:03:03 -0400

    Fix moralis api call to output to console

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// get NFTs for current user on Mainnet
-const userEthNFTs = await Moralis.Web3API.account.getNFTs();
-
-// get testnet NFTs for user
-const testnetNFTs = await Moralis.Web3API.account.getNFTs({ chain: "ropsten" });
-
-// get polygon NFTs for address
-const options = {
-  chain: "Eth",
-  address: "0x0x6BB5971129066120D665034D78B32c1A35bAB55E",
-};
-const polygonNFTs = await Moralis.Web3API.account.getNFTs(options);
+fetch('https://deep-index.moralis.io/api/v2/0x6BB5971129066120D665034D78B32c1A35bAB55E/nft?chain=eth&format=decimal', {
+  method: "GET",
+  headers: {'accept': 'application/json'},
+  headers: {'X-API-Key': 'oeMAODGcFunFg8dBgm5CdgrcfUIA1HZA36o3g0eTxvcP6Pv6OsgHmuGOmXCxTPYU'}
+})
+  .then(response => response.json())
+  .then(json => console.log(json));
