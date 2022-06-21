d5ecdeb9bec94b60038d7c1705a1c25cc85bd6c7
Zion M <zionman33@gmail.com>
2022-06-21 02:48:36 -0400

    Add index.html and modded index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// get NFTs for current user on Mainnet
+const userEthNFTs = await Moralis.Web3API.account.getNFTs();
 
+// get testnet NFTs for user
+const testnetNFTs = await Moralis.Web3API.account.getNFTs({ chain: "ropsten" });
+
+// get polygon NFTs for address
+const options = {
+  chain: "Eth",
+  address: "0x0x6BB5971129066120D665034D78B32c1A35bAB55E",
+};
+const polygonNFTs = await Moralis.Web3API.account.getNFTs(options);
